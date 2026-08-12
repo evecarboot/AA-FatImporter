b8c2765d34a3c2bed6b487e02a8cbacd9be14b76
evecarboot <greenietv@gmail.com>
2026-08-12 22:29:18 +0100

    File Upload

--- aa_fatimporter/hooks.py
+++ aa_fatimporter/hooks.py
@@ -5,7 +5,7 @@ from allianceauth.services.hooks import MenuItemHook
 @hooks.register("menu_item_hook")
 def register_menu():
     return MenuItemHook(
-        text="FAT Importer",
+        text="Import FATs",
         classes="fas fa-file-csv",
         url_name="aa_fatimport_upload",
         navactive=["aa_fatimport_upload"],
