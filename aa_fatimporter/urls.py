d33094bb9ebc4a0ec080f8068dcf2904365b501b
evecarboot <greenietv@gmail.com>
2026-08-13 08:28:19 +0100

    Update urls.py

--- aa_fatimporter/urls.py
+++ aa_fatimporter/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 
 from aa_fatimporter.views import FatImportView, FatLeaderboardView
 
+app_name = "aa_fatimporter"
+
 urlpatterns = [
     path("fat-import/", FatImportView.as_view(), name="aa_fatimport_upload"),
     path("fat-leaderboard/", FatLeaderboardView.as_view(), name="aa_fatimport_dashboard"),
