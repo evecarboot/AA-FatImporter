119d29c8c6be51ff41a4a08bc5fb4686dde44fd4
evecarboot <greenietv@gmail.com>
2026-08-13 02:49:18 +0100

    Update hooks.py

--- aa_fatimporter/hooks.py
+++ aa_fatimporter/hooks.py
@@ -10,11 +10,13 @@ def register_menu():
             classes="fas fa-file-csv",
             url_name="aa_fatimport_upload",
             navactive=["aa_fatimport_upload"],
+            order=100,
         ),
         MenuItemHook(
             text="FAT Dashboard",
             classes="fas fa-chart-line",
             url_name="aa_fatimport_dashboard",
             navactive=["aa_fatimport_dashboard"],
+            order=101,
         ),
     ]
