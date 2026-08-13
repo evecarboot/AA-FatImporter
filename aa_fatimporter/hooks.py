403b9b35b7e0e1555f8553c980cd7400b681a4c4
evecarboot <greenietv@gmail.com>
2026-08-13 02:31:27 +0100

    Update hooks.py

--- aa_fatimporter/hooks.py
+++ aa_fatimporter/hooks.py
@@ -4,17 +4,20 @@ from allianceauth.services.hooks import MenuItemHook
 
 @hooks.register("menu_item_hook")
 def register_menu():
+    admin_access = ["auth.change_user"]
     return [
         MenuItemHook(
             text="Import FATs",
             classes="fas fa-file-csv",
             url_name="aa_fatimport_upload",
             navactive=["aa_fatimport_upload"],
+            access_perms=admin_access,
         ),
         MenuItemHook(
             text="FAT Dashboard",
             classes="fas fa-chart-line",
             url_name="aa_fatimport_dashboard",
             navactive=["aa_fatimport_dashboard"],
+            access_perms=admin_access,
         ),
     ]
