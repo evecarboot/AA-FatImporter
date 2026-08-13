41106152b0a7df7021137a3611163049a32b39d2
evecarboot <greenietv@gmail.com>
2026-08-13 08:24:51 +0100

    Hook Update

--- aa_fatimporter/hooks.py
+++ aa_fatimporter/hooks.py
@@ -1,20 +1,41 @@
-@hooks.register("menu_item_hook")
-def register_import_menu():
-    return MenuItemHook(
-        "Import FATs",
-        "fas fa-file-csv",
-        "aa_fatimport_upload",
-        100,
-        ["aa_fatimport_upload"],
-    )
+from django.utils.translation import gettext_lazy as _
+
+from allianceauth import hooks
+from allianceauth.services.hooks import MenuItemHook, UrlHook
+
+from . import urls
+
+
+class FatImportMenuItem(MenuItemHook):
+    def __init__(self):
+        MenuItemHook.__init__(
+            self,
+            _("Import FATs"),
+            "fas fa-file-csv",
+            "aa_fatimport_upload",
+            navactive=["aa_fatimport_upload"],
+        )
+
+
+class FatDashboardMenuItem(MenuItemHook):
+    def __init__(self):
+        MenuItemHook.__init__(
+            self,
+            _("FAT Dashboard"),
+            "fas fa-chart-line",
+            "aa_fatimport_dashboard",
+            navactive=["aa_fatimport_dashboard"],
+        )
 
 
 @hooks.register("menu_item_hook")
-def register_dashboard_menu():
-    return MenuItemHook(
-        "FAT Dashboard",
-        "fas fa-chart-line",
-        "aa_fatimport_dashboard",
-        101,
-        ["aa_fatimport_dashboard"],
-    )
+def register_menu():
+    return [
+        FatImportMenuItem(),
+        FatDashboardMenuItem(),
+    ]
+
+
+@hooks.register("url_hook")
+def register_urls():
+    return UrlHook(urls, "aa_fatimporter", r"^fat-importer/")
