160bb2312612eaeb7592974bec9bd6541c89c77e
evecarboot <greenietv@gmail.com>
2026-08-13 08:10:10 +0100

    Update hooks.py

--- aa_fatimporter/hooks.py
+++ aa_fatimporter/hooks.py
@@ -1,22 +1,20 @@
-from allianceauth import hooks
-from allianceauth.menu.hooks import MenuItemHook
+@hooks.register("menu_item_hook")
+def register_import_menu():
+    return MenuItemHook(
+        "Import FATs",
+        "fas fa-file-csv",
+        "aa_fatimport_upload",
+        100,
+        ["aa_fatimport_upload"],
+    )
 
 
 @hooks.register("menu_item_hook")
-def register_menu():
-    return [
-        MenuItemHook(
-            "Import FATs",
-            "fas fa-file-csv",
-            "aa_fatimport_upload",
-            100,
-            ["aa_fatimport_upload"],
-        ),
-        MenuItemHook(
-            "FAT Dashboard",
-            "fas fa-chart-line",
-            "aa_fatimport_dashboard",
-            101,
-            ["aa_fatimport_dashboard"],
-        ),
-    ]
+def register_dashboard_menu():
+    return MenuItemHook(
+        "FAT Dashboard",
+        "fas fa-chart-line",
+        "aa_fatimport_dashboard",
+        101,
+        ["aa_fatimport_dashboard"],
+    )
